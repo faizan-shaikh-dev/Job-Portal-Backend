c9d8e32c49b13c54cfa180af8f9bc6933b3c6511
faizan-shaikh-dev <faizangulab4276@gmail.com>
2025-12-11 16:56:45 +0530

    ..

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import jobsRoutes from './routes/jobsRoutes.js';
 import feedRoutes from "./routes/feedRoutes.js"
+import { authToken } from './middleware/auth.js';
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.get('/', (req, res)=>{
 
 app.use('/api/users', authRoutes);
 app.use('/api/jobs', jobsRoutes);
-app.use('/api/feedback', feedRoutes);
+app.use('/api/feedback',authToken, feedRoutes);
 
 const PORT = process.env.PORT || 5000;
 
